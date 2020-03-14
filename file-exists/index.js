9f395fc99f7f87a828c6d08d1ed14477939918ce
Miguel Castillo <migramcastillo@gmail.com>
2020-03-13 21:41:10 -0500

    Day D

---
+++ file-exists/index.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+const exists = file =>
+  new Promise((resolve, reject) => {
+    fs.access(file, err => {
+      if (err) {
+        if (err.code !== "ENOENT") {
+          return reject(err);
+        }
+
+        return resolve({ file, exists: false });
+      }
+
+      resolve({ file, exists: true });
+    });
+  });
+
+exists(process.argv[2])
+  .then(({ file, exists }) => {
+    console.log(`File ${file} ${exists ? "does exist" : "doesnt exist"}`);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
